fa5616126ad2077e679ad43e3711335e572c45c1
brendes6 <143545666+brendes6@users.noreply.github.com>
2024-12-04 20:56:49 -0500

    Update DayTwo.py

--- solutions/DayTwo.py
+++ solutions/DayTwo.py
@@ -2,18 +2,7 @@ import argparse
 import pathlib
 from typing import Iterator
 
-
-# TODO(brendes6): Extract to utilities
-def data_reader(day: str, is_test: bool) -> Iterator[str]:
-    path = pathlib.Path(__file__).parent
-    if is_test:
-        path = path.parent / "inputs" / "Test"
-    else:
-        path = path.parent / "inputs" / "Data"
-    with open(path / f"Day{day.title()}") as f:
-        for line in f:
-            yield (line.rstrip("\n"))
-
+from utilities.py import data_reader
 
 def parse_data(data_reader: Iterator[str]) -> Iterator[Iterator[int]]:
     return (map(int, line.split()) for line in data_reader)
